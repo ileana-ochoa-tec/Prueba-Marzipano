4ea0006e441745394bb188bb8b10ad4a11322ae6
ileana-ochoa-tec <95838830+ileana-ochoa-tec@users.noreply.github.com>
2021-12-08 23:08:37 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-50793253526_81682b8aef_k",
+      "name": "50793253526_81682b8aef_k",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.954677345501338,
+          "pitch": 0.08655130173466041,
+          "title": "Puerta<br>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
